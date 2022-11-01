6c389e01b14eadf44c68e64e78539895e5db079f
Sylphy42 <72261364+Sylphy42@users.noreply.github.com>
2022-10-31 23:03:11 -0300

    Create readline.c

---
+++ readline.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* readline(FILE *data){
+    int size = 10;
+    char *line = malloc(sizeof(char) * size);
+    int i = 0;
+    do{
+        fread(&line[i], sizeof(char), 1, data);
+        i++;
+        if( i == size ){
+            size += 10;
+            line = realloc(line, sizeof(char) * size);
+        }
+    }while( line[i-1] != '\n' && line[i-1] != '\r' && *line[i-1] != '\n\r');
+
+    line[i] = '\0';
+    return line;
+}
+
+int main(int argc, char **argv){
+  FILE *arquivo = fopen(argv[1], "r");
+
+  char *line = readline(arquivo);
+  printf("%s\n", line);
+
+  line = readline(arquivo);
+  printf("%s\n", line);
+
+  line = readline(arquivo);
+  printf("%s\n", line);
+
+  line = readline(arquivo);
+  printf("%s\n", line);
+
+  line = readline(arquivo);
+  printf("%s\n", line);
+}
