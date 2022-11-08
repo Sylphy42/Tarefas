0f73b53664b38318c69b060e45b4fcfec22ea2ba
Sylphy42 <72261364+Sylphy42@users.noreply.github.com>
2022-11-08 19:37:47 -0300

    Update Final.c

--- Final.c
+++ Final.c
@@ -7,9 +7,43 @@
 // printar essas condiçãos de um mesmo ativo juntas na mesma linha, separado por ";"
 // gerar um arquivo que printe essas partes de todos os arquivos que cumpram as condições.
 
+//replace(strtok(NULL,';'), ',','.');
+
 #include <stdio.h>
-#include <cs50.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* readline(FILE *data){
+    int size = 10;
+    char *line = malloc(sizeof(char) * size);
+    int i = 0;
+    do{
+        fread(&line[i], sizeof(char), 1, data);
+        i++;
+        if( i == size ){
+            size += 10;
+            line = realloc(line, sizeof(char) * size);
+        }
+    }while( line[i-1] != '\n' && line[i-1] != '\r'  && line[i-1] != EOF );
+
+    line[i] = '\0';
+    return line;
+}
+
+char strtok(FILE *data){
+    char str[100];
+    char *sub;
+    char texto[20];
+
+    sub = strtok(str, " !,.");
+    while(sub != NULL){
+
+        printf("%s\n",sub);
+        strcmp(texto,sub);
+
+        sub = strtok(NULL," !.,");
+
+    }
 
 typedef struct ativo{
  int prod;
@@ -22,24 +56,29 @@ typedef struct ativo{
 }elem;
 
 int main(){
-    int o;
-    
+    int o, b;
+
  typedef struct ativo elem;
-printf("Como você quer pesquisar no arquivo?\n");
-printf("1 - Valor | 2 - Ativo \n");
-scanf("%i", &o);
- switch(o){
-    case 1:
-printf ("Privet\n");
-    break;
+ FILE *arquivo = fopen("temp.csv", "r");
+ fprintf("%i", ativo);
 
-    case 2:
-printf ("Hallo\n");
-    break;
+// printf("Como você quer pesquisar no arquivo?\n");
+// printf("1 - Valor | 2 - Ativo \n");
+// scanf("%i", &o);
+//  switch(o){
+//     case 1:
+// printf ("Qual o valor mínimo dos ativos que deseja procurar?\n");
+// scanf("%i", &b);
+//     break;
 
-    default:
-    printf("Valor inválido");
-    break;
-}
+//     case 2:
+// printf ("Qual  ativo deseja procurar?\n");
+// scanf("%i", &b);
+//     break;
+
+//     default:
+//     printf("Valor inválido");
+//     break;
+// }
 
 }
